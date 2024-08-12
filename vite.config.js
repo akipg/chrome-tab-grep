eccce179f31e4c2f3deafd0d1a4b4e47e2648103
akipg <108157287+akipg@users.noreply.github.com>
2024-08-12 13:54:06 +0900

    config vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
-import { defineConfig } from 'vite'
+import { defineConfig, normalizePath  } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    viteStaticCopy({
+      targets: [
+        { src: normalizePath(path.resolve(__dirname, './manifest.json')), dest: '' },
+        { src: normalizePath(path.resolve(__dirname, './static')), dest: '' }
+      ]
+    })
   ],
   resolve: {
     alias: {
